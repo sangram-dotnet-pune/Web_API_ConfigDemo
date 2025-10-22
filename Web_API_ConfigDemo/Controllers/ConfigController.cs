b52f569f867139a38fdb9745494d637b50181548
sangram-dotnet-pune <sangramcanva7812@gmail.com>
2025-10-22 17:54:47 +0530

    Add project files.

---
+++ Web_API_ConfigDemo/Controllers/ConfigController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web_API_ConfigDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConfigController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public ConfigController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        public IActionResult GetAppSettings()
+        {
+            var appName = _config["AppSettings:ApplicationName"];
+            var version = _config["AppSettings:Version"];
+
+            return Ok(new { appName, version });
+        }
+    }
+}
